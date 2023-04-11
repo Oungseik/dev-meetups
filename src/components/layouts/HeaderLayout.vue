d0567be65f48101b2e9bd85e1237465072aad37e
Oungseik <mhemaungthuwin@gmail.com>
2023-04-11 09:40:49 +0630

    feat: create nav bar

--- src/components/layouts/HeaderLayout.vue
+++ src/components/layouts/HeaderLayout.vue
@@ -4,13 +4,13 @@ import SideNavLayout from "./SideNavLayout.vue";
 
 import { ref } from "vue";
 
-const sideNav = ref(false);
+const drawer = ref(false);
 
 const navItems = [
   {
     title: "View Meetups",
     to: "/meetups",
-    icon: "heroicons:users-solid",
+    icon: "mdi-account-supervisor",
   },
   {
     title: "Organize Meetup",
@@ -37,10 +37,15 @@ const navItems = [
 
 <template>
   <v-toolbar elevation="4">
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     <v-toolbar-title>DevMeetup</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
-      <v-btn v-for="item in navItems" :key="item.title">{{ item.title }}</v-btn>
+      <v-btn v-for="item in navItems" :key="item.title">
+        <v-icon :icon="item.icon" start />
+        {{ item.title }}
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
+  <v-navigation-drawer v-model="drawer" temporary></v-navigation-drawer>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,17 +5,30 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-import "iconify-icon";
+// import "iconify-icon";
 
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import { fa } from "vuetify/iconsets/fa";
+
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
 });
 
 const app = createApp(App);
